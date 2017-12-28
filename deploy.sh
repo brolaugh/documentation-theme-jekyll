c85eef8c038ee0825d6220b24fa6cb4ab73a895b
Hannes Kindströmmer <hannes.kindstrommer@ip1.se>
2017-12-28 16:24:58 +0100

    Change to script deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ] && [  !$TRAVIS_PULL_REQUEST ]; then
-  find _site -type f -exec curl --ftp-create-dirs -T {} -u $AZURE_WA_USERNAME:$AZURE_WA_PASSWORD $AZURE_WA_SITE/site/wwwroot/{} \;
-else
-    echo "Was not on branch master or was pull request, not publishing..."
-fi
+find _site -type f -exec curl --ftp-create-dirs -T {} -u $AZURE_WA_USERNAME:$AZURE_WA_PASSWORD $AZURE_WA_SITE/site/wwwroot/{} \;
