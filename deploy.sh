3e845d814c329acbd7720afcc68395a514fe4d22
Hannes Kindströmmer <hannes.kindstrommer@ip1.se>
2018-01-02 11:01:25 +0100

    Upload to the correct folder

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 bundle install
 bundle exec jekyll build
-find _site -type f -exec curl -v --ftp-create-dirs -T {} -u $AZURE_WA_USERNAME:$AZURE_WA_PASSWORD $AZURE_WA_SITE/site/wwwroot/{} \;
+cd _site
+find . -type f -exec curl --ftp-create-dirs -T {} -u $AZURE_WA_USERNAME:$AZURE_WA_PASSWORD $AZURE_WA_SITE/site/wwwroot/{} \;
